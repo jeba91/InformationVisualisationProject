98f3bd2676155c5da316e1db290b46e991710d24
Joe Harrison <joeharrison1994@gmail.com>
2019-03-05 15:43:13 +0100

    Pictures now showing

--- request.php
+++ request.php
@@ -8,10 +8,16 @@
     $return_arr = array();
     while ($row = mysqli_fetch_assoc($result)) {
         $id = $row['id'];
+        $photo_id = $row['photo_id'];
+        $server = $row['server'];
+        $farm = $row['farm'];
+        $secret = $row['secret'];
+
         $title = $row['title'];
         $longitude = $row['longitude'];
         $latitude = $row['latitude'];
-        $url = $row['url'];
+        $url = 'http://farm' . $farm . '.staticflickr.com/' . $server . '/' . $photo_id . '_' . $secret . '.jpg';
+
         $return_arr[] = array('id' => $id,'title' => $title, 'longitude' => $longitude, 'latitude' => $latitude, 'url' => $url);
     }
     header('Content-Type: application/json');
