06d56519e7fa6b72fcea9837ec516d496fb9caf3
jeba91 <jeroen769@gmail.com>
2019-03-21 14:56:46 +0100

    Graph change button

--- graph2version2.js
+++ graph2version2.js
@@ -23,7 +23,7 @@ function clean_text(txt){
 }
 
 
-function build_graph(photo1, photo2){
+function build_graph_cat(photo1, photo2){
     let data = {
         nodes: [{id: "Photo1", type: "photo1"}],
         links: []
@@ -119,11 +119,11 @@ function build_graph(photo1, photo2){
 
     }
     console.log(data)
-    build_from_data(data)
+    build_from_data_cat(data)
 }
 
 
-function build_graph_cat_lab_cat(photo1, photo2){
+function build_graph_lab(photo1, photo2){
 
     let data = {
         nodes: [{id: "Photo1", type: "photo1"}],
@@ -214,17 +214,11 @@ function build_graph_cat_lab_cat(photo1, photo2){
         });
     }
     console.log(data.links)
-    build_from_data(data);
+    build_from_data_lab(data);
 }
 
 
-
-
-
-
-
-
-function build_from_data(data){
+function build_from_data_cat(data){
 
     let diagram_width = parseInt(d3.select("#chart svg").style('width').replace("px", ""))
     let sankey = d3.sankey().size([diagram_width, 490])
@@ -372,4 +366,120 @@ function build_from_data(data){
                   .attr("text-anchor", d => d.x0 < 500 / 2 ? "start" : "end")
                   .text(d => clean_text(d.id));
         }
+}
+
+function build_from_data_lab(data){
+    console.log(data)
+
+    let diagram_width = parseInt(d3.select("#chart svg").style('width').replace("px", ""))
+    let sankey = d3.sankey().size([diagram_width, 490])
+    .nodeId(d => d.id)
+    .nodeWidth(20)
+    .nodePadding(10)
+    .nodeAlign(d3.sankeyCenter);
+
+    let graph = sankey(data);
+
+
+
+    graph.nodes.forEach(function(d){
+        if(d.type == "category1"){
+            d.x0 = diagram_width*0.25;
+            d.x1 = diagram_width*0.25 + 20;
+
+        }
+        if(d.type == "category2"){
+            d.x0 = diagram_width*0.75;
+            d.x1= diagram_width*0.75 + 20;
+        }
+        if(d.type == "label"){
+            d.x0 = diagram_width*0.5;
+            d.x1 = diagram_width*0.5 + 20;
+        }
+        // if(d.type == "photo1" || d.type=="photo2"){
+        //     d.y0 = 0;
+        //     d.y1 = 500;
+        // }
+    });
+
+    graph_svg.selectAll('*').remove();
+
+    let defs = graph_svg.append('defs')
+
+    let links = graph_svg.append("g")
+        .selectAll("path")
+        .data(graph.links)
+        .enter()
+        .append("path")
+        .attr("d", d3.sankeyLinkHorizontal())
+        .attr("fill", "none")
+        .attr("stroke", "#606060")
+        .attr("stroke-width", function(d) {
+        return d.width - 10})
+        .attr("stroke-opacity", 0.4)
+        .on("mouseover", function() {
+                d3.select(this).style("stroke-opacity", "0.7")
+                })
+        .on("mouseout", function() {
+                d3.select(this).style("stroke-opacity", "0.4")
+        });
+
+
+    let nodes = graph_svg.append("g")
+        .selectAll("rect")
+        .data(graph.nodes)
+        .enter()
+        .append("rect")
+        .attr("x", d => d.x0)
+        .attr("y", d => d.y0)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0)
+        .attr("fill", d => d.color = color(clean_text(d.id)))
+        .style("stroke", function(d){
+            return d3.rgb(d.color).darker(2)
+        })
+        .attr("opacity", 0.8);
+
+
+        links.style('stroke', (d, i) => {
+           const gradientID = i;
+           const startColor = d.source.color
+           const stopColor = d.target.color
+           const linearGradient = defs.append('linearGradient')
+               .attr('id', gradientID)
+               .attr("gradientUnits", "userSpaceOnUse");
+
+           linearGradient.selectAll('stop')
+             .data([
+                 {offset: '0%', color: startColor },
+                 {offset: '100%', color:stopColor}
+                 // {offset: '50%', color: stopColor },
+                 // {offset: '20%', color: startColor },
+                 // {offset: '50%', color: stopColor },
+                 // {offset: '30%', color: startColor },
+                 // {offset: '90%', color: stopColor }
+               ])
+             .enter().append('stop')
+             .attr('offset', d => {
+               return d.offset;
+             })
+             .attr('stop-color', d => {
+               return d.color;
+             });
+           return `url(#${gradientID})`;
+         })
+
+
+    let text = graph_svg.append("g")
+        .style("font", "10px sans-serif")
+        .selectAll("text")
+        .data(graph.nodes)
+        .join("text")
+        .attr("x", d => d.x0 < 500 / 2 ? d.x1 + 6 : d.x0 - 6)
+        .attr("y", d => (d.y1 + d.y0) / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", d => d.x0 < 500 / 2 ? "start" : "end")
+        .text(d => clean_text(d.id));
+
+
 }
